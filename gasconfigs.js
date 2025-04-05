70521324513fee7870b20b840594447f93fe9c47
alkihuri <alkihuri@gmail.com>
2025-04-05 22:52:18 +0200

    Update gasconfigs.js

--- gasconfigs.js
+++ gasconfigs.js
@@ -1,4 +1,4 @@
-window.PROD_SCRIPT_URL=  'https://script.google.com/macros/s/AKfycbywYGlqrw0Q32aVBV78rAVyeBApfjxjqV3IhGkHYWptC6msOHwWgH_kR1NLKEpRLFVT/exec';
+window.PROD_SCRIPT_URL=  'https://script.google.com/macros/s/AKfycbxhkiChVfuaBiL4A2hcb-4787C9k3YnFj5W99998lMfhQiNEJtMefjtUb5ftHopU6EL/exec';
 window.DEV_SCRIPT_URL=  'https://script.google.com/macros/s/AKfycbywYGlqrw0Q32aVBV78rAVyeBApfjxjqV3IhGkHYWptC6msOHwWgH_kR1NLKEpRLFVT/exec';
  
  
