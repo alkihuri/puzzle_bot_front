bc134c0ca87945003b25e8ac50753e49eff9b0fa
alkihuri <alkihuri@gmail.com>
2025-04-06 15:28:36 +0200

    articles form edit

--- formlogic.js
+++ formlogic.js
@@ -1,133 +1,155 @@
- 
-    // URL вашего веб-приложения Google Apps Scriptconst 
-    const SCRIPT_URL =  'https://script.google.com/macros/s/AKfycbz6A5P3mWRDj0QN5TlOWEoONL_COMM8iYmzbUFvl-IQ_McRERR300RbZvZuf9FoPSUu/exec'; // Замените на ваш URL скрипта
-    const form = document.getElementById('articleForm');
-    const messageEl = document.getElementById('message');
-    const loadingEl = document.querySelector('.loading');
-    const imageInput = document.getElementById('image');
-    const imagePreview = document.getElementById('imagePreview');
 
-    // Предпросмотр изображения
-    imageInput.addEventListener('change', function() {
-      const file = this.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-          imagePreview.src = e.target.result;
-          imagePreview.style.display = 'block';
-        }
-        reader.readAsDataURL(file);
-      }
-    });
+// URL вашего веб-приложения Google Apps Scriptconst 
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz6A5P3mWRDj0QN5TlOWEoONL_COMM8iYmzbUFvl-IQ_McRERR300RbZvZuf9FoPSUu/exec'; // Замените на ваш URL скрипта
+const form = document.getElementById('articleForm');
+const messageEl = document.getElementById('message');
+const loadingEl = document.querySelector('.loading');
+const imageInput = document.getElementById('image');
+const imagePreview = document.getElementById('imagePreview');
 
-    // Отправка формы
-    form.addEventListener('submit', async function(e) {
-      e.preventDefault();
-      
-         // Если запущено в Telegram - берем реальные данные
-        if (window.Telegram && Telegram.WebApp) {
-          const tg = Telegram.WebApp;
-          const tgUser = tg.initDataUnsafe.user;
-          
-          if (tgUser) {
-            userData = {
-              telegram_id: tgUser.id,
-              name: [tgUser.first_name, tgUser.last_name].filter(Boolean).join(' '),
-              study: 'Telegram User'
-            };
-          }
-        } 
-        else
-        {
-          // Данные по умолчанию (для теста вне Telegram)
-          userData = {
-            telegram_id: 'Нет данных об пользователе',
-            name: 'No user',
-            study: 'No data'
-          };
-        }
+// Предпросмотр изображения
+imageInput.addEventListener('change', function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      imagePreview.src = e.target.result;
+      imagePreview.style.display = 'block';
+    }
+    reader.readAsDataURL(file);
+  }
+});
 
-      var telegram_user;
+// Отправка формы
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
 
-      try {
-        telegram_user = userData.telegram_id + ' ' + userData.name;
-      }
-      catch (error) {
-        console.error('Ошибка:', error);
-        telegram_user = 'Нет данных об пользователе';
-      }
+  var userData = {};  
+  // Если запущено в Telegram - берем реальные данные
+  if (window.Telegram && Telegram.WebApp) {
+    const tg = Telegram.WebApp;
+    const tgUser = tg.initDataUnsafe.user;
 
-      const formData = {
-        title: document.getElementById('title').value.trim(),
-        text: document.getElementById('text').value.trim(),
-        study: document.getElementById('study').value.trim(),
-        type: document.getElementById('type').value,
-        telegram_user : telegram_user
+    if (tgUser) {
+      userData = {
+        telegram_id: tgUser.id,
+        name: [tgUser.first_name, tgUser.last_name].filter(Boolean).join(' '),
+        study: 'Telegram User'
       };
-      console.log('Отправка данных 123 :',  formData);  
-      // Валидация
-      if (!formData.title || !formData.text || !formData.type) {
-        showMessage('Пожалуйста, заполните все обязательные поля', 'error');
-        return;
-      }
+    }
+  }
+  else {
+    // Данные по умолчанию (для теста вне Telegram)
+    userData = {
+      telegram_id: 'Нет данных об пользователе',
+      name: 'No user',
+      study: 'No data'
+    };
+  }
 
-      toggleLoading(true);
+  var telegram_user;
 
-      try {
- 
-          await submitRegularForm(formData);
-        
-      } 
-      catch (error) {
-        console.error('Error:', error);
-        showMessage(`Ошибка: ${error.message}`, 'error');
-      } finally {
-        toggleLoading(false);
-      }
+  try {
+    if (window.Telegram && Telegram.WebApp) {
+      telegram_user = userData.telegram_id + ' ' + userData.name;
+    }
+  }
+  catch (error) {
+    console.error('Ошибка:', error);
+    telegram_user = 'Нет данных об пользователе';
+  }
+
+
+  var directions = [];
+
+  // Получаем выбранные направления 
+
+  if(document.getElementById("uiuxbox").checked) 
+  {
+    directions.push('UI/UX');
+  }
+  if(document.getElementById("backendbox").checked) 
+  {
+    directions.push('backend'); 
+  }
+  if(document.getElementById("frontendbox").checked)
+  {
+    directions.push('frontend');
+  }
+
+  console.log('Выбранные направления:', directions.join(', '));  
+
+
+  const formData = {
+    title: document.getElementById('title').value.trim(),
+    text: document.getElementById('text').value.trim(),
+    study:  directions.join(', '),
+    type: document.getElementById('type').value,
+    telegram_user: telegram_user
+  };
+  console.log('Отправка данных 123 :', formData);
+  // Валидация
+  if (!formData.title || !formData.text || !formData.type) {
+    showMessage('Пожалуйста, заполните все обязательные поля', 'error');
+    return;
+  }
+
+  toggleLoading(true);
+
+  try {
+
+    await submitRegularForm(formData);
+
+  }
+  catch (error) {
+    console.error('Error:', error);
+    showMessage(`Ошибка: ${error.message}`, 'error');
+  } finally {
+    toggleLoading(false);
+  }
+});
+
+// Стандартная отправка формы
+async function submitRegularForm(formData) {
+
+
+
+  console.log('Отправка данных:', JSON.stringify(formData));
+
+  try {
+
+    // Отправка данных на сервер
+    const response = await fetch(SCRIPT_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Allow': '*',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      },
+      body: JSON.stringify(formData),
+      mode: 'no-cors'
     });
 
-    // Стандартная отправка формы
-    async function submitRegularForm(formData) {
- 
- 
 
-      console.log('Отправка данных:', JSON.stringify(formData));
+  } catch (error) {
+    console.error('Ошибка отправки:', error);
+    showMessage(`Ошибка отправки: ${error.message}`, 'error');
+  }
+}
 
-      try { 
+function showMessage(text, type = 'success') {
+  messageEl.className = `alert alert-${type}`;
+  messageEl.textContent = text;
+  messageEl.style.display = 'block';
 
-        // Отправка данных на сервер
-        const response = await fetch(SCRIPT_URL, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Allow': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-          },
-          body: JSON.stringify(formData) ,
-          mode: 'no-cors'
-        });
-        
-         
-      } catch (error) {
-        console.error('Ошибка отправки:', error);
-        showMessage(`Ошибка отправки: ${error.message}`, 'error');
-      }
-    }
+  setTimeout(() => {
+    messageEl.style.display = 'none';
+  }, 5000);
+}
 
-    function showMessage(text, type = 'success') {
-      messageEl.className = `alert alert-${type}`;
-      messageEl.textContent = text;
-      messageEl.style.display = 'block';
-      
-      setTimeout(() => {
-        messageEl.style.display = 'none';
-      }, 5000);
-    }
-
-    function toggleLoading(show) {
-      loadingEl.style.display = show ? 'block' : 'none';
-    }
- 
+function toggleLoading(show) {
+  loadingEl.style.display = show ? 'block' : 'none';
+}
